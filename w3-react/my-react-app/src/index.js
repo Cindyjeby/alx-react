7f3b9e88e733341ba3c37adc9fd0cf8ada436daa
cindyjeby <cindyjeby@gmail.com>
2024-08-30 12:08:30 +0300

    how to make a function component and render it

--- w3-react/my-react-app/src/index.js
+++ w3-react/my-react-app/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const x = 50
-
-const myFirstElement = <h1>{(x) < 10 ? "Hello" : "Goodbye"}</h1>
+function Car() {
+    return <h2>Hi, I am a car</h2>
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(myFirstElement);
+root.render(<Car />);
+
