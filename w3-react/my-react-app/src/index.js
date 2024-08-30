5cb08490f8edeb7788c0909ff3710b0f82e1b796
cindyjeby <cindyjeby@gmail.com>
2024-08-30 12:29:16 +0300

    components inside another component

--- w3-react/my-react-app/src/index.js
+++ w3-react/my-react-app/src/index.js
@@ -2,7 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 function Car() {
-    return <h2>Hi, I am a car</h2>
+    return (
+        <>
+            <h2>Hi, I am a car</h2>
+            <Garage/>
+        </>
+    );
+}
+
+function Garage() {
+    return (
+        <>
+            <h3>How many car are in here?</h3>
+        </>
+    );
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
