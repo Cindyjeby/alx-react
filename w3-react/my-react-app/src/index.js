9c88adb9a465cd2035e132e7bec4fb90bfaa561b
cindyjeby <cindyjeby@gmail.com>
2024-09-02 10:37:16 +0300

    useState:how to use it, read it and update it

--- w3-react/my-react-app/src/index.js
+++ w3-react/my-react-app/src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {useState} from "react";
 
-// conditional rednering using ternary operator
+function Car() {
+    const [car, setCar] = useState({
+        brand: 'Ford',
+        model: "Mustang",
+        Year: "1964",
+        color: "red"
+    });
+    const updateColor = () => {
+        setCar(previousState => {
+            return { ...previousState, color: "blue"}
+        });
+    }
 
-function MyForm() {
     return (
-        <form>
-            <label>Enter your name:
-                <input type="text" />
-            </label>
-        </form>
+        <>
+            <h1> My {car.brand}</h1>
+            <p>
+                it is a{car.color} {car.model} from {car.year}
+            </p>
+            <button
+            type="button"
+            onClick={updateColor}
+            >Blue</button>
+        </>
     )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<MyForm />);
+root.render(<Car />);
