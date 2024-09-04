39809e9b9b6bdcb94b60cb73f0a318aa9a219887
cindyjeby <cindyjeby@gmail.com>
2024-09-04 10:02:14 +0300

    useStae to create a textarea

--- w3-react/my-react-app/src/index.js
+++ w3-react/my-react-app/src/index.js
@@ -3,36 +3,17 @@ import ReactDOM from 'react-dom/client';
 import {useState} from "react";
 
 function MyForm() {
-    const [inputs, setInputs] = useState({});
+    const [textarea, setTextarea] = useState(
+        "the content of a text area goes in the value attribute"
+    );
 
     const handleChange = (event) => {
-        const name = event.target.name;
-        const value = event.target.value;
-        setInputs(values => ({...values, [name]: value}))
+        setTextarea(event.target.value)
     }
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        alert(inputs);
-    }
+
     return (
-        <form onSubmit={handleSubmit}>
-            <label>Enter your name:
-                <input
-                type="text"
-                name="username"
-                value={inputs.username || ""}
-                onChange={handleChange}
-                />
-            </label>
-            <label> Enter your age:
-                <input
-                type="number"
-                name="age"
-                value={inputs.age || ""}
-                onChange={handleChange}
-                />
-            </label>
-            <inputs type="submit" />
+        <form>
+            <textarea value={textarea} onChange={handleChange} />
         </form>
     )
 }
