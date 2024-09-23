3458189cc42244bd73742fb5fab10a251f4e851a
cindyjeby <cindyjeby@gmail.com>
2024-09-23 13:43:40 +0300

    made a submition form for logging in

--- 0x01-react_intro/task_0/dashboard/src/App.js
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -7,15 +7,33 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src='/images/holberton.jpg' className='App-logo' alt="logo" />
-	  <h1>School Dashboard</h1>
+	      <h1>School Dashboard</h1>
       </header>
-	<body className="App-body">
-	  <p>Login to access the full dashboard</p>
-	</body>
-	<footer className="App-footer">
-	  <p>Copyright {getFullYear()} - {getFooterCopy}</p>
-	  </footer>
-    </div>
+
+      <body className="App-body">
+        <p>Login to access the full dashboard</p>
+
+        <form>
+          <div className='container'>
+          <div>
+            <label htmlFor='email'>Email: </label>
+            <input type='email' id='email' />
+          </div>
+          <div>
+            <label htmlFor='password'>Password: </label>
+            <input type='password' id='password' />
+          </div>
+          </div>
+          <div className="btn">
+            <button>OK</button>
+          </div>
+        </form>
+      </body>
+
+      <footer className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy}</p>
+        </footer>
+        </div>
   );
 }
 
